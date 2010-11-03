7b729ece09eae9d4cc51ce30afee58794ce8050b
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2010-11-03 12:00:57 +0100

    added the possibility to set your own open-vm-tools version

--- manifests/classes/base.pp
+++ manifests/classes/base.pp
@@ -39,10 +39,13 @@ class openvmtools {
 
       # RHEL4 only has an glib2-2.4 version. We must stay with the latest
       # version which doesn't require glib2-2.6.
-      $ovt_version = $lsbmajdistrelease ? {
-        "4"     => "2009.01.21-142982",
-        "6"     => "2010.10.18-313025",
-        default => "2009.07.22-179896",
+      $ovt_version = $openvmtools_version ? { 
+	      "" => $lsbmajdistrelease ? {
+          "4"     => "2009.01.21-142982",
+          "6"     => "2010.10.18-313025",
+          default => "2009.07.22-179896",
+        },
+        default => $openvmtools_version,
       }
 
       # curiously open-vm-tools build system links to a non-existing file...
